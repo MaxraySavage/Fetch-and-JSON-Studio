189ccb72dee4183853e3f4ca8b34b566d589eaea
Maxray Savage <MaxraySavage@users.noreply.github.com>
2021-08-13 15:17:42 -0400

    begin building loop to append astronaut html

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // TODO: add code here
+
+window.addEventListener('load', () => {
+    fetch('https://handlers.education.launchcode.org/static/astronauts.json').then((response) => {
+        response.json().then((json) => {
+            const astronautArray = json;
+            console.log(astronautArray[0]);
+            const astronautContainer = document.getElementById('container');
+            let astronautDocFragment= document.createDocumentFragment();
+            for(const astronaut of astronautArray) {
+                const astronautDiv = document.createElement('div');
+                astronautDiv.classList.add('astronaut');
+                astronautDocFragment.appendChild(astronautDiv);
+                const bioDiv = document.createElement('div');
+                bioDiv.classList.add('bio');
+                astronautDiv.appendChild(bioDiv);
+                
+                const profileImg = document.createElement('img');
+            }
+        });
+    });
+});
